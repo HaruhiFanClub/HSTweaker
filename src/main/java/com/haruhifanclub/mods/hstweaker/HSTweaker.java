53eb8381ef3dd3120d70e66e2efeba6f5c0b4315
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-01 10:56:16 +0800

    feat: 初始化开发环境

---
+++ src/main/java/com/haruhifanclub/mods/hstweaker/HSTweaker.java
@@ -0,0 +1,13 @@
+package com.haruhifanclub.mods.hstweaker;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(HSTweaker.MOD_ID)
+public class HSTweaker {
+
+    public static final String MOD_ID = "hstweaker";
+
+    public HSTweaker() {}
+
+}
+
