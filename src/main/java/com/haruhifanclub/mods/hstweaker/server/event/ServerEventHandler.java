a825c662c50cb9357140a1364ed31232295b9ff7
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-01 22:50:38 +0800

    feat: 食用腐肉不饥饿或食用毒马铃薯不中毒时给予10秒幸运Ⅰ效果

--- src/main/java/com/haruhifanclub/mods/hstweaker/server/event/ServerEventHandler.java
+++ src/main/java/com/haruhifanclub/mods/hstweaker/server/event/ServerEventHandler.java
@@ -1,4 +1,20 @@
 package com.haruhifanclub.mods.hstweaker.server.event;
 
+import org.auioc.mods.ahutils.server.event.impl.LivingEatAddEffectEvent;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
 public class ServerEventHandler {
+
+    @SubscribeEvent
+    public static void onEatAddEffect(final LivingEatAddEffectEvent event) {
+        Item food = event.getFood().getItem();
+        if ((food == Items.ROTTEN_FLESH || food == Items.POISONOUS_POTATO) && event.getEffects().size() == 0) {
+            event.getEffects().add(new MobEffectInstance(MobEffects.LUCK, 200, 0, false, false, true));
+        }
+    }
+
 }
