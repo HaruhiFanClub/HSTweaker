f97a46a102b4949999dcaafcc9980567ca65562a
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-01 20:41:56 +0800

    feat: ServerEventHandler

--- src/main/java/com/haruhifanclub/mods/hstweaker/HSTweaker.java
+++ src/main/java/com/haruhifanclub/mods/hstweaker/HSTweaker.java
@@ -1,5 +1,8 @@
 package com.haruhifanclub.mods.hstweaker;
 
+import com.haruhifanclub.mods.hstweaker.server.event.ServerEventHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(HSTweaker.MOD_ID)
@@ -7,7 +10,16 @@ public class HSTweaker {
 
     public static final String MOD_ID = "hstweaker";
 
-    public HSTweaker() {}
+    public HSTweaker() {
+        final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+
+        forgeSetup(forgeEventBus);
+    }
+
+
+    private void forgeSetup(final IEventBus forgeEventBus) {
+        forgeEventBus.register(ServerEventHandler.class);
+    }
 
 }
 
---
+++ src/main/java/com/haruhifanclub/mods/hstweaker/server/event/ServerEventHandler.java
@@ -0,0 +1,4 @@
+package com.haruhifanclub.mods.hstweaker.server.event;
+
+public class ServerEventHandler {
+}
