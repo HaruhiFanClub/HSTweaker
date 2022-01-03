a2b072856225965f650188bffde30007aa41dd6b
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-03 10:15:25 +0800

    feat: 添加长效,强化幸运药水及酿造配方

--- src/main/java/com/haruhifanclub/mods/hstweaker/common/potion/PotionRegistry.java
+++ src/main/java/com/haruhifanclub/mods/hstweaker/common/potion/PotionRegistry.java
@@ -1,11 +1,13 @@
 package com.haruhifanclub.mods.hstweaker.common.potion;
 
 import com.haruhifanclub.mods.hstweaker.HSTweaker;
+import com.haruhifanclub.mods.hstweaker.common.potion.impl.LuckPotion;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class PotionRegistry {
 
@@ -16,6 +18,12 @@ public class PotionRegistry {
         event.enqueueWork(PotionRegistry::registerBrewingRecipes);
     }
 
-    private static void registerBrewingRecipes() {}
+    public static final RegistryObject<Potion> LONG_LUCK = LuckPotion.Long.register();
+    public static final RegistryObject<Potion> STRONG_LUCK = LuckPotion.Strong.register();
+
+    private static void registerBrewingRecipes() {
+        LuckPotion.Long.registerBrewingRecipe();
+        LuckPotion.Strong.registerBrewingRecipe();
+    }
 
 }
---
+++ src/main/java/com/haruhifanclub/mods/hstweaker/common/potion/impl/LuckPotion.java
@@ -0,0 +1,45 @@
+package com.haruhifanclub.mods.hstweaker.common.potion.impl;
+
+import com.haruhifanclub.mods.hstweaker.common.potion.PotionRegistry;
+import com.haruhifanclub.mods.hstweaker.common.potion.base.HTPotion;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraftforge.registries.RegistryObject;
+
+public class LuckPotion extends HTPotion {
+
+    protected LuckPotion(int duration, int amplifier) {
+        super("luck", MobEffects.LUCK, duration, amplifier);
+    }
+
+    public static class Long extends LuckPotion {
+        protected Long() {
+            super(10 * 60 * 20, 0);
+        }
+
+        public static RegistryObject<Potion> register() {
+            return register("long_luck", Long::new);
+        }
+
+        public static boolean registerBrewingRecipe() {
+            return registerBrewingRecipe(Potions.LUCK, Items.REDSTONE, PotionRegistry.LONG_LUCK);
+        }
+    }
+
+    public static class Strong extends LuckPotion {
+        protected Strong() {
+            super((int) 2.5 * 60 * 20, 1);
+        }
+
+        public static RegistryObject<Potion> register() {
+            return register("strong_luck", Strong::new);
+        }
+
+        public static boolean registerBrewingRecipe() {
+            return registerBrewingRecipe(Potions.LUCK, Items.GLOWSTONE_DUST, PotionRegistry.STRONG_LUCK);
+        }
+    }
+
+}
