3b3f47af03a039e9f7173d5f4b4b445a13307957
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-12 23:46:35 +0800

    feat: HSTEventHandler

--- src/main/java/com/haruhifanclub/hstweaker/HSTweaker.java
+++ src/main/java/com/haruhifanclub/hstweaker/HSTweaker.java
@@ -1,6 +1,8 @@
 package com.haruhifanclub.hstweaker;
 
+import com.haruhifanclub.hstweaker.server.event.HSTEventHandler;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +16,8 @@ public class HSTweaker {
 
     public HSTweaker() {
         ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (s, b) -> true));
+
+        MinecraftForge.EVENT_BUS.register(HSTEventHandler.class);
     }
 
     public static ResourceLocation id(String path) {
---
+++ src/main/java/com/haruhifanclub/hstweaker/server/event/HSTEventHandler.java
@@ -0,0 +1,11 @@
+package com.haruhifanclub.hstweaker.server.event;
+
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public final class HSTEventHandler {
+
+    @SubscribeEvent
+    public static void registerCommands(final RegisterCommandsEvent event) {}
+
+}
