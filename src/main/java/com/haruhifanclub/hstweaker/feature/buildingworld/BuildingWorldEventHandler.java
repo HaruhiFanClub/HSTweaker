4f7657258c8ee18c91761f701ded058df8f762fd
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-13 22:10:41 +0800

    feat: only handle server event

--- src/main/java/com/haruhifanclub/hstweaker/feature/buildingworld/BuildingWorldEventHandler.java
+++ src/main/java/com/haruhifanclub/hstweaker/feature/buildingworld/BuildingWorldEventHandler.java
@@ -6,25 +6,27 @@ import net.minecraft.Util;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.GameType;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.world.ExplosionEvent;
 
 public class BuildingWorldEventHandler {
 
     public static void onPlayerChangedDimension(final PlayerEvent.PlayerChangedDimensionEvent event) {
-        if (event.getPlayer() instanceof ServerPlayer player) {
-            if (player.createCommandSourceStack().hasPermission(3)) return;
-            if (BuildingWorld.isThis(event.getTo())) {
-                player.setGameMode(GameType.CREATIVE);
-            } else if (BuildingWorld.isThis(event.getFrom())) {
-                player.getInventory().clearContent();
-                player.setExperienceLevels(0);
-                player.setExperiencePoints(0);
-                player.removeAllEffects();
-                player.setGameMode(GameType.SURVIVAL);
-            }
+        var player = (ServerPlayer) event.getPlayer();
+        if (player.createCommandSourceStack().hasPermission(3)) return;
+        if (BuildingWorld.isThis(event.getTo())) {
+            player.setGameMode(GameType.CREATIVE);
+        } else if (BuildingWorld.isThis(event.getFrom())) {
+            player.getInventory().clearContent();
+            player.setExperienceLevels(0);
+            player.setExperiencePoints(0);
+            player.removeAllEffects();
+            player.setGameMode(GameType.SURVIVAL);
         }
     }
+
+    public static void onEntityJoinWorld(final EntityJoinWorldEvent event) {}
 
     public static void onExplosionStart(final ExplosionEvent.Start event) {
         if (BuildingWorld.isThis(event.getWorld())) {
--- src/main/java/com/haruhifanclub/hstweaker/server/event/HSTEventHandler.java
+++ src/main/java/com/haruhifanclub/hstweaker/server/event/HSTEventHandler.java
@@ -4,11 +4,14 @@ import org.auioc.mcmod.arnicalib.common.event.impl.PistonCheckPushableEvent;
 import com.haruhifanclub.hstweaker.feature.antiduplication.AntiDuplicationEventHandler;
 import com.haruhifanclub.hstweaker.feature.buildingworld.BuildingWorldEventHandler;
 import com.haruhifanclub.hstweaker.server.command.HSTCommands;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+@SuppressWarnings("resource")
 public final class HSTEventHandler {
 
     @SubscribeEvent
@@ -16,19 +19,27 @@ public final class HSTEventHandler {
         HSTCommands.register(event.getDispatcher());
     }
 
-
     @SubscribeEvent
     public static void onPlayerChangedDimension(final PlayerEvent.PlayerChangedDimensionEvent event) {
+        if (!(event.getPlayer() instanceof ServerPlayer)) return;
         BuildingWorldEventHandler.onPlayerChangedDimension(event);
     }
 
     @SubscribeEvent
+    public static void onEntityJoinWorldDimension(final EntityJoinWorldEvent event) {
+        if (event.getWorld().isClientSide) return;
+        BuildingWorldEventHandler.onEntityJoinWorld(event);
+    }
+
+    @SubscribeEvent
     public static void onExplosionStart(final ExplosionEvent.Start event) {
+        if (event.getWorld().isClientSide) return;
         BuildingWorldEventHandler.onExplosionStart(event);
     }
 
     @SubscribeEvent
     public static void onPistonCheckPushable(final PistonCheckPushableEvent event) {
+        if (event.getWorld().isClientSide()) return;
         AntiDuplicationEventHandler.onPistonCheckPushable(event);
     }
 
