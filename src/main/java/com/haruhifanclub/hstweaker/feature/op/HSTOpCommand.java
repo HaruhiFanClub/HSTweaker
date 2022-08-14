7495fde88e98601b19cf684ab2b62c30a6a06ed1
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-15 02:02:43 +0800

    feat(op): removeRespawnPosition

--- src/main/java/com/haruhifanclub/hstweaker/feature/op/HSTOpCommand.java
+++ src/main/java/com/haruhifanclub/hstweaker/feature/op/HSTOpCommand.java
@@ -1,13 +1,28 @@
 package com.haruhifanclub.hstweaker.feature.op;
 
+import static net.minecraft.commands.Commands.argument;
 import static net.minecraft.commands.Commands.literal;
 import org.auioc.mcmod.arnicalib.utils.game.CommandUtils;
 import com.mojang.brigadier.tree.CommandNode;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.world.level.Level;
 
 public class HSTOpCommand {
 
-    public static final CommandNode<CommandSourceStack> NODE = literal("op").requires(CommandUtils.PERMISSION_LEVEL_3).build();
+    public static final CommandNode<CommandSourceStack> NODE = literal("op").requires(CommandUtils.PERMISSION_LEVEL_3)
+        .then(
+            literal("removeRespawnPosition")
+                .then(
+                    argument("players", EntityArgument.players())
+                        .executes((ctx) -> {
+                            var players = EntityArgument.getPlayers(ctx, "players");
+                            for (var player : players) player.setRespawnPosition(Level.OVERWORLD, null, 0.0F, false, false);
+                            return players.size();
+                        })
+                )
+        )
+        .build();
 
     public static void register(final CommandNode<CommandSourceStack> parent) {
         parent.addChild(NODE);
