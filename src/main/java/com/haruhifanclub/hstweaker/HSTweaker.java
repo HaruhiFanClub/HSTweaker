ac6b1aba14daab3dd67f6da0cb49914dfe358937
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-15 10:37:08 +0800

    feat: version command

--- src/main/java/com/haruhifanclub/hstweaker/HSTweaker.java
+++ src/main/java/com/haruhifanclub/hstweaker/HSTweaker.java
@@ -1,6 +1,11 @@
 package com.haruhifanclub.hstweaker;
 
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.auioc.mcmod.arnicalib.utils.LogUtil;
 import org.auioc.mcmod.arnicalib.utils.game.TextUtils;
+import org.auioc.mcmod.arnicalib.utils.java.VersionUtils;
 import com.haruhifanclub.hstweaker.feature.world.HSTWorldEventDispatcher;
 import com.haruhifanclub.hstweaker.server.event.HSTEventHandler;
 import net.minecraft.ChatFormatting;
@@ -18,11 +23,24 @@ public class HSTweaker {
     public static final String MOD_ID = "hstweaker";
     public static final String MOD_NAME = "HSTweaker";
 
+    public static final String MAIN_VERSION;
+    public static final String FULL_VERSION;
+
+    public static final Logger LOGGER = LogUtil.getLogger(MOD_NAME);
+    private static final Marker CORE = LogUtil.getMarker("CORE");
+
     public HSTweaker() {
         ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (s, b) -> true));
 
         MinecraftForge.EVENT_BUS.register(HSTEventHandler.class);
         MinecraftForge.EVENT_BUS.register(HSTWorldEventDispatcher.class);
+    }
+
+    static {
+        Pair<String, String> version = VersionUtils.getModVersion(HSTweaker.class);
+        MAIN_VERSION = version.getLeft();
+        FULL_VERSION = version.getRight();
+        LOGGER.info(CORE, "Version: " + MAIN_VERSION + " (" + FULL_VERSION + ")");
     }
 
     public static ResourceLocation id(String path) {
--- src/main/java/com/haruhifanclub/hstweaker/server/command/HSTCommands.java
+++ src/main/java/com/haruhifanclub/hstweaker/server/command/HSTCommands.java
@@ -2,6 +2,7 @@ package com.haruhifanclub.hstweaker.server.command;
 
 import static net.minecraft.commands.Commands.literal;
 import java.util.function.Function;
+import org.auioc.mcmod.arnicalib.common.command.impl.VersionCommand;
 import org.auioc.mcmod.arnicalib.utils.game.CommandFeedbackHelper;
 import com.haruhifanclub.hstweaker.HSTweaker;
 import com.haruhifanclub.hstweaker.feature.op.HSTOpCommand;
@@ -15,6 +16,7 @@ public final class HSTCommands {
     public static final CommandNode<CommandSourceStack> NODE = literal(HSTweaker.MOD_ID).build();
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        VersionCommand.addVersionNode(NODE, HSTweaker.class);
         HSTOpCommand.register(NODE);
         HSTWorldCommands.register(NODE);
 
