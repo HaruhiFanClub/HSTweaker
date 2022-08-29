6f57acec00931c1094af900477fc9da3bfab5d1f
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-29 23:46:03 +0800

    fix(anti): NoInteractingAboveCeiling ignore ops

--- src/main/java/com/haruhifanclub/hstweaker/feature/anti/ceiling/NoInteractingAboveCeiling.java
+++ src/main/java/com/haruhifanclub/hstweaker/feature/anti/ceiling/NoInteractingAboveCeiling.java
@@ -1,5 +1,6 @@
 package com.haruhifanclub.hstweaker.feature.anti.ceiling;
 
+import org.auioc.mcmod.arnicalib.utils.game.PlayerUtils;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -9,6 +10,7 @@ public class NoInteractingAboveCeiling {
     @SubscribeEvent
     public static void onBlockPlace(final BlockEvent.EntityPlaceEvent event) {
         if (event.getEntity() instanceof ServerPlayer player) {
+            if (PlayerUtils.isOp(player)) return;
             if (Ceiling.isNearCeiling(player.getLevel(), event.getPos().getY(), 0.0D)) {
                 Ceiling.warn(player, "no_placing_block_above", "place block above bedrock ceiling", event.getPos().toString());
                 event.setCanceled(true);
