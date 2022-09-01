a71a93f5f533dc50ad245c062ef556045eb790ae
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-02 01:06:23 +0800

    feat(anti): bedrock end platform

--- src/main/java/com/haruhifanclub/hstweaker/mixin/server/MixinServerLevel.java
+++ src/main/java/com/haruhifanclub/hstweaker/mixin/server/MixinServerLevel.java
@@ -2,30 +2,24 @@ package com.haruhifanclub.hstweaker.mixin.server;
 
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
-@Mixin(value = ServerPlayer.class)
-public class MixinServerPlayer {
-
-    @Overwrite()
-    public boolean shouldFilterMessageTo(ServerPlayer p_143422_) {
-        return false;
-    }
+@Mixin(value = ServerLevel.class)
+public class MixinServerLevel {
 
     @Redirect(
-        method = "Lnet/minecraft/server/level/ServerPlayer;createEndPlatform(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;)V",
+        method = "Lnet/minecraft/server/level/ServerLevel;lambda$makeObsidianPlatform$21(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;)V",
         at = @At(
             value = "FIELD",
             target = "Lnet/minecraft/world/level/block/Blocks;OBSIDIAN:Lnet/minecraft/world/level/block/Block;",
             opcode = Opcodes.GETSTATIC
         )
     )
-    private Block redirect_createEndPlatform_getObsidianBlock() {
+    private static Block redirect_makeObsidianPlatform_getObsidianBlock() {
         return Blocks.BEDROCK;
     }
 
